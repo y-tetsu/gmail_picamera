fae3521c7d6b0fd9c921dfc9edc4826afb0e1169
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-03-13 18:16:09 +0900

    Gmail送信追加

--- gmail/gmail.py
+++ gmail/gmail.py
@@ -3,8 +3,17 @@
 # gmail-controller
 #==================
 
-import os
+import pickle
+import os.path
 import json
+
+import base64
+from email.mime.text import MIMEText
+from apiclient import errors
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 
 class Gmail:
@@ -13,16 +22,22 @@ class Gmail:
     """
     def __init__(self, setting_file):
         self.setting = self.load_setting(setting_file)
+        self.scope = ['https://www.googleapis.com/auth/gmail.send']
 
     def load_setting(self, setting_file):
         """
         loading setting file
         """
         setting = {
-            "user_addresses": [
-                "USER_ADDRESS1",
-                "USER_ADDRESS2"
-            ]
+            "sender_address": "SENDER_ADDRESS",
+            "to_addresses": [
+                "TO_ADDRESS1",
+                "TO_ADDRESS2"
+            ],
+            "token_pickle": "TOKEN_PICKLE",
+            "credential": "CREDENTIAL",
+            "subject": "SUBJECT",
+            "message": "MESSAGE"
         }
 
         if os.path.isfile(setting_file):
@@ -31,9 +46,60 @@ class Gmail:
 
         return setting
 
+    def send(self, to_index=None):
+        """
+        send gmail
+        """
+        token_pickle = self.setting["token_pickle"]
+        credential = self.setting["credential"]
+
+        creds = None
+        if os.path.exists(token_pickle):
+            with open(token_pickle, 'rb') as token:
+                creds = pickle.load(token)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(credential, self.scope)
+                creds = flow.run_local_server()
+
+            with open(token_pickle, 'wb') as token:
+                pickle.dump(creds, token)
+
+        service = build('gmail', 'v1', credentials=creds)
+        message = self._create_message(to_index)
+        self._send_message(service, 'me', message)
+
+    def _create_message(self, to_index):
+        """
+        create mesasge
+        """
+        sender = self.setting["sender_address"]
+        to = self.setting["to_addresses"][to_index]
+        subject = self.setting["subject"]
+        message_text = self.setting["message"]
+
+        message = MIMEText(message_text)
+        message['from'] = sender
+        message['to'] = to
+        message['subject'] = subject
+        encode_message = base64.urlsafe_b64encode(message.as_bytes())
+
+        return {'raw': encode_message.decode()}
+
+    def _send_message(self, service, user_id, message):
+        """
+        send mesasge
+        """
+        try:
+            message = (service.users().messages().send(userId=user_id, body=message).execute())
+            print('Message Id: %s' % message['id'])
+            return message
+        except errors.HttpError as error:
+            print('An error occurred: %s' % error)
+
 
 if __name__ == '__main__':
-    import getpass
-
     gmail = Gmail('./setting.json')
-    print(gmail.setting)
+    gmail.send(to_index=0)
