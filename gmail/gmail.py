70ffc12360197e3c5934db83cd0abc5244986dcb
y-tetsu <45305605+y-tetsu@users.noreply.github.com>
2020-03-13 10:51:58 +0900

    暗号化削除

--- gmail/gmail.py
+++ gmail/gmail.py
@@ -6,33 +6,28 @@
 import os
 import json
 
-import crypt_setting
-
 
 class Gmail:
     """
     gmail-controller
     """
-    def __init__(self, enc_setting, password):
-        self.setting = self.load_setting(enc_setting, password)
+    def __init__(self, setting_file):
+        self.setting = self.load_setting(setting_file)
 
-    def load_setting(self, enc_setting, password):
+    def load_setting(self, setting_file):
         """
         loading setting file
         """
         setting = {
-            "login_name": "LOGIN_NAME",
-            "login_pass": "LOGIN_PASSWARD",
             "user_addresses": [
                 "USER_ADDRESS1",
                 "USER_ADDRESS2"
             ]
         }
 
-        if os.path.isfile(enc_setting):
-            with open(enc_setting, 'rb') as f:
-                bintext = crypt_setting.decrypt(f.read(), password)
-                setting = json.loads(bintext.decode())
+        if os.path.isfile(setting_file):
+            with open(setting_file) as f:
+                setting = json.load(f)
 
         return setting
 
@@ -40,6 +35,5 @@ class Gmail:
 if __name__ == '__main__':
     import getpass
 
-    password = getpass.getpass('password > ')
-    gmail = Gmail('./enc_setting.json', password)
+    gmail = Gmail('./setting.json')
     print(gmail.setting)
